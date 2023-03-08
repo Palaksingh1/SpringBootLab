96e047d82b9d29f260f53e2deb95b0864f54acec
Palaksingh1 <117344679+Palaksingh1@users.noreply.github.com>
2023-03-08 19:29:02 +0530

    Add files via upload

---
+++ ThursdayLabAmitavaSir/WebApplication/WebApplication.java
@@ -0,0 +1,12 @@
+package edu.learning.thursdaylab.logic;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+public class WebApplication {
+@GetMapping(value = "/anudipweb")
+	
+	public String demo() {
+		return "<a href='https://www.anudip.org/'>"
+				+ "<body bgcolor='black' text='white'><center><h1>Anudip </h1></center></body></a>";
+}
+}
