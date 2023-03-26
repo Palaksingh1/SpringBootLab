371201e9f090f5bb42f36597e41f0e896f7b987a
Palaksingh1 <117344679+Palaksingh1@users.noreply.github.com>
2023-03-26 10:43:50 +0530

    Add files via upload

---
+++ ThursdayLabAmitavaSir/subjectdependency/configuration/DependencyConfiguration.java
@@ -0,0 +1,25 @@
+package subject.dependency.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import subject.dependency.entity.Subject;
+import subject.dependency.entity.Trainer;
+
+@Configuration
+public class DependencyConfiguration {
+
+	@Bean
+	public Subject subject ()
+	{
+       return new Subject ("AI");
+  
+    }
+	
+	@Bean
+	public Trainer trainer ()
+	{
+       return new Trainer (subject());
+  
+    }
+}
